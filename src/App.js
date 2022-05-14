9ff336b7ba4f0ea8b7daeff0a8b25ed0c4939f97
RamGoel <rgoel766@gmail.com>
2022-05-14 16:50:29 +0530

    frontend done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import './style.css';
-import Login from './components/Login/Login';
+// import Login from './components/Login/Login';
+import { initializeApp } from "firebase/app";
 import Home from './components/Home/Home';
 import NavBar from './components/navBar/NavBar';
+// import firebaseConfig from "./config.js";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-export default function App() {
-  return (
-    <div>
-      {/* <Login /> */}
-      <NavBar />
-      <Home />
-    </div>
-  );
+
+export default class App extends React.Component {
+  constructor(){
+    super(props)
+    // initializeApp(firebaseConfig);
+  }
+
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Home />
+      </div>
+    );
+  }
 }
+
---
+++ src/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBn1e0PGELtdE8ISWkKgL633JCS_lZRNWI",
+  authDomain: "taxfiler-2835d.firebaseapp.com",
+  projectId: "taxfiler-2835d",
+  storageBucket: "taxfiler-2835d.appspot.com",
+  messagingSenderId: "749929160558",
+  appId: "1:749929160558:web:cf7ec272d7dfedf9f9ab7f",
+  measurementId: "G-RWBQ7NGDQ8"
+};
+
+
+export default firebaseConfig
